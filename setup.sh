8c79607623b54e5b474580a7a9e1a6f111802f7c
Shaked Klein Orbach <klein.shaked@gmail.com>
2014-02-24 16:43:11 +0100

    Adds git configuration support inlcuding setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# Vim
 ln -sf "${DIR}/.vimrc" $HOME/.vimrc
 ln -sf "${DIR}/vim/" $HOME/vim
+
+# Git
+ln -sf "${DIR}/.gitconfig" $HOME/.gitconfig
